97955129d0b3d1897fd317f04d8e4a660da2eeef
Tarang Jain <40517122+tarang-jain@users.noreply.github.com>
2020-08-28 10:09:32 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 #Flags
-preprocess=0               #set to 1 if data processing needed
-encode_data=0              #set to 1 if embeddings are not already created and saved
+preprocess=1               #set to 1 if data processing needed
+encode_data=1              #set to 1 if embeddings are not already created and saved
 run_question_answering=1   #set to 1 to converse with the model
-run_eval=1                 #set to 1 to run evaluation
+run_eval=0                 #set to 1 to run evaluation
 
 question_beam=50 #number of <question, answer> pairs to be searched
 
 printf "\n"
-echo "Starting bert service in new terminal window ..."
 
-#if you have a bert service already running, you should first close it
-
-gnome-terminal -e "bash -c \"bert-serving-start -model_dir ../BertModels/uncased_L-12_H-768_A-12 -num_worker 1 -max_seq_len 40; exec bash\""
-#set the model_dir as the directory where your bert model is placed
+#First run bert-as-service in new terminal window
 
 if [ $preprocess -eq 1 ]; then
 printf "\n"
@@ -27,20 +23,21 @@ python3 preprocess.py
 mv "cornell movie-dialogs corpus" "cornell_movie-dialogs_corpus"
 corpus_dir="cornell_movie-dialogs_corpus"
 
-#Check whether eval files are created; if not create them
-if [ ! -f $corpus_dir/preprocessed_q_eval.txt ]; then
-	printf "\n"
-	echo "Preparing eval questions ..."
-	tail -n 11283 $corpus_dir/preprocessed_q.txt > $corpus_dir/preprocessed_q_eval.txt
-fi
+printf "\n"
+echo "Preparing eval questions ..."
+eval_len=5000
+total_len=$(cat $corpus_dir/preprocessed_q.txt|wc -l)
+training_len=$((total_len - eval_len))
+tail -n $eval_len $corpus_dir/preprocessed_q.txt > $corpus_dir/preprocessed_q_eval.txt
+head -n $training_len $corpus_dir/preprocessed_q.txt > $corpus_dir/preprocessed_q_train.txt
 
-if [ ! -f $corpus_dir/preprocessed_qa_eval.txt ]; then
-        printf "\n"
-        echo "Preparing eval <question, answer> pairs ..."
-        tail -n 11283 $corpus_dir/preprocessed_qa.txt > $corpus_dir/preprocessed_qa_eval.txt
-fi
+printf "\n"
+echo "Preparing eval answers ..."
+tail -n $eval_len $corpus_dir/preprocessed_qa.txt > $corpus_dir/preprocessed_qa_eval.txt
+head -n $training_len $corpus_dir/preprocessed_qa.txt > $corpus_dir/preprocessed_qa_train.txt
 
 mv "cornell_movie-dialogs_corpus" "cornell movie-dialogs corpus"
+
 
 fi
 
@@ -54,20 +51,21 @@ python3 encode_data.py
 
 fi
 
+
 if [ $run_question_answering -eq 1 ]; then
 printf "\n"
 echo ============================================================================
-echo "            Run question answering using 'question beam                   "
+echo "            Run question answering using question beam                   "
 echo ============================================================================
 
 python3 converse.py --beam=$question_beam
 
 fi
-
+                                                                                                                                                                                          61,2          Bot
 if [ $run_eval -eq 1 ]; then
 printf "\n"
 echo ============================================================================
-echo "                         Cosine Similarity Test                           "
+echo "                          Cosine Similarity Test                          "
 echo ============================================================================
 
 python3 eval.py --beam=$question_beam
